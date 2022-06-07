75e0b38015b6097f2def6238a1abc099c4ec4b76
zhLisha <lisazhou1709@gmail.com>
2022-06-07 15:00:35 +0200

    correct milestone-2 javascript "space"

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -13,7 +13,7 @@ for( let i = 1; i <= 100; i++ ) {
     let numberFuzzAndBuzz = i;
 
     // Differenziare tra i multipli di 3 e 5, solo di 3 e solo di 5
-    if( i % 3 ===0 && i % 5 === 0) {
+    if( i % 3 === 0 && i % 5 === 0) {
         numberFuzzAndBuzz = 'FizzBuzz'
     } else if ( i % 5 === 0) {
         numberFuzzAndBuzz = 'Buzz';
